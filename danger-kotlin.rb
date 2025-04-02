fcccd6475b881c421083427b62d0c98c6f9f6dd8
r0adkll <veedubusc@gmail.com>
2025-04-02 09:08:28 -0400

    combining dk taps into single file

--- danger-kotlin.rb
+++ danger-kotlin.rb
@@ -2,17 +2,35 @@ class DangerKotlin < Formula
   desc "Write your Dangerfiles in Kotlin"
   homepage "https://github.com/r0adkll/danger-kotlin"
   version "2.0.7"
-  url "https://github.com/r0adkll/danger-kotlin/releases/download/#{version}/danger-kotlin-macosArm64.tar"
-  sha256 "869d676a9b62efeac47e6b42f4da634d1d39807be9d15ae76a4fde0e60d67ae0"
   head "https://github.com/r0adkll/danger-kotlin.git"
+  
+  if Hardware::CPU.arm?
+    url "https://github.com/r0adkll/danger-kotlin/releases/download/#{version}/danger-kotlin-macosArm64.tar"
+    sha256 "869d676a9b62efeac47e6b42f4da634d1d39807be9d15ae76a4fde0e60d67ae0"
+  
+    # Use the vendored danger
+    depends_on "danger/tap/danger-js"
+    depends_on "kotlin"
 
-  # Use the vendored danger
-  depends_on "danger/tap/danger-js"
-  depends_on "kotlin"
+    def install
+      libexec.install %w[bin]
+      prefix.install %w[lib]
+      (bin/"danger-kotlin").write_env_script libexec/"bin/danger-kotlin", Language::Java.overridable_java_home_env
+    end
+  end
 
- def install
-    libexec.install %w[bin]
-    prefix.install %w[lib]
-    (bin/"danger-kotlin").write_env_script libexec/"bin/danger-kotlin", Language::Java.overridable_java_home_env
- end
+  if Hardware::CPU.intel?
+    url "https://github.com/r0adkll/danger-kotlin/releases/download/#{version}/danger-kotlin-macosX64.tar"
+    sha256 "d8011232835d2fe09a848c0439a63a54d8ccc619d0aab42361844863eceeb068"
+
+    # Use the vendored danger
+    depends_on "danger/tap/danger-js"
+    depends_on "kotlin"
+
+    def install
+      libexec.install %w[bin]
+      prefix.install %w[lib]
+      (bin/"danger-kotlin").write_env_script libexec/"bin/danger-kotlin", Language::Java.overridable_java_home_env
+    end
+  end
 end
