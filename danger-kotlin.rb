e079e13016cc886d902bd97d9b978f99ab8b1a30
r0adkll <veedubusc@gmail.com>
2025-10-29 15:15:07 -0400

    Updating danger-kotlin to 2.1.0

--- danger-kotlin.rb
+++ danger-kotlin.rb
@@ -1,12 +1,12 @@
 class DangerKotlin < Formula
   desc "Write your Dangerfiles in Kotlin"
   homepage "https://github.com/r0adkll/danger-kotlin"
-  version "2.0.8"
+  version "2.1.0"
   head "https://github.com/r0adkll/danger-kotlin.git"
   
   if Hardware::CPU.arm?
     url "https://github.com/r0adkll/danger-kotlin/releases/download/#{version}/danger-kotlin-macosArm64.tar"
-    sha256 "e2fb770555f07293e1edc432c063e99f711826b31bc930ce6b7df6d1b4a1f215"
+    sha256 "795702ce82ec39fff9fbca580eb25564fb03a0c4f298a804db2d53c854820fcc"
   
     # Use the vendored danger
     depends_on "danger/tap/danger-js"
@@ -21,7 +21,7 @@ class DangerKotlin < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/r0adkll/danger-kotlin/releases/download/#{version}/danger-kotlin-macosX64.tar"
-    sha256 "7257fe8c4087494b30c5dc59547c5312ccbae07d4c84d04f8a30c813f5a1b3a9"
+    sha256 "a33c2193371fe2dbdd6ad17791493ea7cf820a3436802dc3963809b7c640dd2d"
 
     # Use the vendored danger
     depends_on "danger/tap/danger-js"
