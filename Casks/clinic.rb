244d20b9b9f52c9cfccbe02e4ecba612bbf03edf
r0adkll <veedubusc@gmail.com>
2026-09-19 13:06:56 -0400

    clinic 0.2.0

--- Casks/clinic.rb
+++ Casks/clinic.rb
@@ -1,11 +1,11 @@
 # Written by scripts/publish in r0adkll/clinic for each release; edit it there.
 cask "clinic" do
-  version "0.1.0"
-  sha256 "5664cc09465b3d6381fa68158a1510b498ad50e8a77d5711c2593319edb1bf7b"
+  version "0.2.0"
+  sha256 "a2cf7542b324942ef4de150d9da334c7444f3a4a2ef34f5c4d8cf03960212031"
 
   url "https://github.com/r0adkll/clinic/releases/download/#{version}/Clinic-#{version}.zip"
   name "Clinic"
-  desc "Claude Code session manager on libghostty"
+  desc "Session manager for Claude Code"
   homepage "https://github.com/r0adkll/clinic"
 
   livecheck do
